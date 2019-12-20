128323cc254f68ea81c41ec221912042684d15ed
Hzy0913 <hanzhaoy@chanjet.com>
2019-12-20 17:04:15 +0800

    feat: update types

--- types.ts
+++ types.ts
@@ -7,7 +7,7 @@ export interface Options {
   filter?: (any) => boolean;
   hotKeyConfig: HotKeyConfigType;
   operationControl: OperationControlType;
-  clickBefore?: () => void;
+  clickBefore?: (keyName, node) => void | boolean;
 }
 
 export type FindFunc = (selector: string, isNew?: boolean) => QueryNodeType;
@@ -15,6 +15,7 @@ export type NextFunc = (filterNum?: number) => QueryNodeType;
 export type PrevFunc = (filterNum?: number) => QueryNodeType;
 export type SetFocusFunc = (target: HTMLElement) => void;
 export type ClearFocus = (target: HTMLElement) => void;
+export type QueryNodeFunc = (target: HTMLElement) => any;
 export type RegisterFunc = (keyName: string) => (selectors: string) =>
   (targetSelectors: string[]) => void;
 
@@ -31,7 +32,7 @@ export interface QueryNodeType {
   prev: PrevFunc;
   setFocus: SetFocusFunc;
   clearFocus: ClearFocus;
-  click: () => void;
+  click: (keyName: string, node: QueryNodeType) => void;
 }
 
 export interface ControllerType {
@@ -39,15 +40,15 @@ export interface ControllerType {
   readonly hotKeyConfig: HotKeyConfigType;
   readonly operationControl: OperationControlType;
   readonly log: boolean;
-  action: (keyName: string, callback?: (any) => void) => void;
+  action: (keyName: string, callback?: (node, index, keyName) => void) => void;
   find: FindFunc;
   observerCallback: (any) => void;
   pressed: (any) => void;
   observerTrigger: () => void;
   bind: (keys: string[], callback: (any) => void) => void;
   unbind: (keys: string[]) => string[];
-  on: (callback: (any) => void) => void;
-  off: (callback: (any) => void) => void;
+  on: (callback: (target, index, keyName) => void) => void;
+  off: (callback: (target, index, keyName) => void) => void;
   setFocus: SetFocusFunc;
   getFocusId: () => string | number;
   register: RegisterFunc;
@@ -59,7 +60,7 @@ export interface ControllerType {
 export interface HotKeyConfigType {
   [keyName: string]: {
     describe: string;
-    operation?: string[];
+    operation?: string[] | string[][];
     dynamic?: boolean;
   };
 }
